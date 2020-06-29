cd00e0a1d4d553fb7a2c08d8859c2621108fd861
caterina <catemarian1806@gmail.com>
2020-06-29 15:41:39 +0200

    parola palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,11 @@
 var parola = prompt("Inserisci una parola");
+var rovescio = "";
+for (var i = 0; i < parola.length; i++) {
+  rovescio += parola[(parola.length -1 -i)]
+ }
+console.log(parola , rovescio);
+if (parola == rovescio) {
+  console.log("Hai inserito una parola palindroma");
+} else {
+  console.log("La parola inserita non è palindroma");
+}
